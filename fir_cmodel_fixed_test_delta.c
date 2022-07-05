85107dd21587a7c1c28e957005a2b3f1f2cc83e2
York Tsang <york.tsang@atom-semiconductor.com>
2022-07-05 09:53:16 +0800

    updated

--- fir_cmodel_fixed_test_delta.c
+++ fir_cmodel_fixed_test_delta.c
@@ -4,15 +4,16 @@
 #include <stdio.h>
 #include "mac_fixed.h"
 #include "fir_cmodel_fixed.h"
-#define COEF_LEN 5
-#define DATA_LEN 14
+#define COEF_LEN 3
+#define DATA_LEN 5
 #define OUTPUT_LEN (DATA_LEN-COEF_LEN+1)
-#define INPUT_IS_DOUBLE 0
+#define INPUT_IS_DOUBLE 1
+#define GAIN 7
 int main(void)
 {
   int i = 0;
   #if INPUT_IS_DOUBLE==1
-    double double_coefs[COEF_LEN] = {0.25, -0.5, 0.75};
+    double double_coefs[COEF_LEN] = {0.9, 0.9, -0.9};
     double double_data[DATA_LEN] = {0, 0, -0.234, 0, 0};
     uint16_t q115_coefs[COEF_LEN];
     uint16_t q115_data[DATA_LEN];
@@ -28,13 +29,13 @@ int main(void)
   #endif
   
   uint16_t q115_output[OUTPUT_LEN];
-  int gain = 0;
-  fixed_fir(q115_data, q115_coefs, q115_output, DATA_LEN, COEF_LEN, gain, 1);
+
+  fixed_fir(q115_data, q115_coefs, q115_output, OUTPUT_LEN, COEF_LEN, GAIN, 1);
   double double_output[OUTPUT_LEN];
   q115_array_to_double_array(q115_output, double_output, OUTPUT_LEN);
   if (SAT_FLAG)
     printf("Overflow detected!\n");
-  printf("gain=%d\n", gain);
+  printf("gain=%d\n", GAIN);
   printf("double coef array: ");
   for (i = 0; i < COEF_LEN; i++)
     printf("%.10f ", double_coefs[i]);
